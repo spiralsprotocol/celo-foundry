f9b813593cf1260bcd56afb10a543c1d5a399b19
Douglas Qian <douglaszqian@gmail.com>
2022-08-31 01:09:28 -0400

    Support solidity version pragma ^0.8.0

--- script/Helper.s.sol
+++ script/Helper.s.sol
@@ -1,21 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.5.13;
+pragma solidity ^0.8.0;
 
-import "forge-std/Script.sol";
+// import "forge-std/Script.sol";
 import "../src/PrecompileHandler.sol";
 
-contract Helper is Script, PrecompileHandler {
-  function run() public {
-    console.log("Precompiles Address");
-    console.log(address(this));
-    string memory transferSig = "transfer(address,address,uint256)";
-    bytes4 sig = bytes4(keccak256(abi.encodePacked(transferSig)));
-    console.log(transferSig);
-    console.logBytes4(sig);
+contract Helper is PrecompileHandler {
+    function run() public {
+        console.log("Precompiles Address");
+        console.log(address(this));
+        string memory transferSig = "transfer(address,address,uint256)";
+        bytes4 sig = bytes4(keccak256(abi.encodePacked(transferSig)));
+        console.log(transferSig);
+        console.logBytes4(sig);
 
-    console.log("transfer ProxyCode");
-    bytes memory resp = proxyTo(sig);
-    console.logBytes(resp);
-    vm.etch(address(0xff - 2), resp);
-  }
+        console.log("transfer ProxyCode");
+        bytes memory resp = proxyTo(sig);
+        console.logBytes(resp);
+        vm.etch(address(0xff - 2), resp);
+    }
 }
--- src/Test.sol
+++ src/Test.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.5.13;
+pragma solidity ^0.8.0;
 
 import {Test as ForgeTest} from "forge-std/Test.sol";
 import "./Precompiles.sol";
@@ -9,41 +9,33 @@ contract Test is ForgeTest, Precompiles {
     PrecompileHandler public ph;
     address currentPrank;
 
-    event log_named_array(string key, address[] val);
+    // TODO delete me
+    // event log_named_array(string key, address[] val);
     event log_named_array(string key, bytes32[] val);
     event log_array(string key, address[] val);
     event log_array(string key, bytes32[] val);
 
-    constructor() ForgeTest() public {
+    constructor() ForgeTest() {
         ph = new PrecompileHandler();
     }
 
     /* Utility functions */
 
-    function changePrank(address who) internal {
-      // Record current prank so helper functions can revert
-      // if they need to prank
-      currentPrank = who;
-      super.changePrank(who);
-    }
+    // function changePrank(address who) internal override {
+    //     // Record current prank so helper functions can revert
+    //     // if they need to prank
+    //     currentPrank = who;
+    //     super.changePrank(who);
+    // }
 
     function actor(string memory name) public returns (address) {
-      uint256 pk = uint256(keccak256(bytes(name)));
-      address addr = vm.addr(pk);
-      vm.label(addr, name);
-      return addr;
+        uint256 pk = uint256(keccak256(bytes(name)));
+        address addr = vm.addr(pk);
+        vm.label(addr, name);
+        return addr;
     }
 
     /* Extra assertions, extends forge-std/Test.sol */
-
-    function assertEq(address[] memory a, address[] memory b) internal {
-        if (keccak256(abi.encode(a)) != keccak256(abi.encode(b))) {
-            emit log("Error: a == b not satisfied [address[]]");
-            emit log_named_array("  Expected", b);
-            emit log_named_array("    Actual", a);
-            fail();
-        }
-    }
 
     function assertEq(bytes32[] memory a, bytes32[] memory b) internal {
         if (keccak256(abi.encode(a)) != keccak256(abi.encode(b))) {
